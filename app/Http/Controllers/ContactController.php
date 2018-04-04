dce470639e2ec4754861cd50cbb822ea4162a251
aksenovandrei <aksenov.andrew@gmail.com>
2018-04-04 12:05:14 +0300

    Добавил функционал записи данных с контактной формы в БД

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Order;
+use Illuminate\Support\Facades\DB;
 
 class ContactController extends Controller
 {
@@ -12,10 +14,19 @@ class ContactController extends Controller
     }
     public function store(Request $request){
 
-        $userName = $request->input('name');
+        $order = new Order();
+
+        $order->name = $request->name;
+        $order->email = $request->email;
+        $order->phone = $request->contact_number;
+        $order->text = $request->message;
+
+        $order->save();
 
 
-        $request->session()->put('userName', $userName);
+
+
+        $request->session()->put('userName', $request->name);
         return redirect()->route('thanks');
 
     }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,6 +16,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Schema::defaultStringLength(191); // без это записий не работает миграция
+
+        //прослушивание - как только будет выполняться любой sql запрос - метод listen вызовет на исполнение эту функцию
+        DB::listen(function ($query){
+            dump($query->sql);
+            dump($query->bindings);
+        });
     }
 
     /**
