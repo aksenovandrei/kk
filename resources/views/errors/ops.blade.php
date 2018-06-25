ee1208346343ee6ba5db86c8b4e3adbe568d462e
aksenovandrei <aksenov.andrew@gmail.com>
2018-06-25 12:15:28 +0300

    Добавил вьюху с ошибкой

---
+++ resources/views/errors/ops.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.layout')
+@section('title', 'Упс')
+@include('includes.sub-menu')
+@include('includes.footer')
+@section('thanks')
+    <h1>Что-то пошло не так...</h1>
+    <h2>Попробуйте еще раз через пару минут или напишите нам на почту content.kvartira@gmail.com</h2>
+@stop
